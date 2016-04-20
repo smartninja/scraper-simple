4b58fe480e8e617463f2a48177bf4aab8e246e8f
Matej Ramuta <matej.ramuta@gmail.com>
2016-04-20 19:49:32 +0200

    initial commit

---
+++ scrapebook.py
@@ -0,0 +1,17 @@
+import BeautifulSoup
+import urllib2
+
+url = 'https://scrapebook22.appspot.com/'
+
+html = urllib2.urlopen(url).read()
+
+soup = BeautifulSoup.BeautifulSoup(html)
+
+print soup.html.head.title.string  # should be: Scrapebook | by SmartNinja
+
+for link in soup.findAll("a"):
+    if link.string == "See full profile":
+        person_url = "https://scrapebook22.appspot.com" + link["href"]
+        person_html = urllib2.urlopen(person_url).read()
+        person_soup = BeautifulSoup.BeautifulSoup(person_html)
+        print person_soup.find("span", attrs={"class": "email"}).string
