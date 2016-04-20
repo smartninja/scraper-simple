a74beb6c3a3534b32646421250d1eae323f25789
Matej Ramuta <matej.ramuta@gmail.com>
2016-04-20 22:00:41 +0200

    simplifying readibility by calling specific functions from libraries

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import BeautifulSoup
-import urllib2
+from BeautifulSoup import BeautifulSoup
+from urllib2 import urlopen
 
 url = 'https://scrapebook22.appspot.com/'
 
-response = urllib2.urlopen(url).read()
+response = urlopen(url).read()
 
-soup = BeautifulSoup.BeautifulSoup(response)
+soup = BeautifulSoup(response)
 
 print soup.html.head.title.string  # should be: Scrapebook | by SmartNinja
 
 for link in soup.findAll("a"):
     if link.string == "See full profile":
         person_url = "https://scrapebook22.appspot.com" + link["href"]
-        person_html = urllib2.urlopen(person_url).read()
-        person_soup = BeautifulSoup.BeautifulSoup(person_html)
+        person_html = urlopen(person_url).read()
+        person_soup = BeautifulSoup(person_html)
         print person_soup.find("span", attrs={"class": "email"}).string
